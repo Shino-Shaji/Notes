63701d6892f5548fd16f6d3c231f8e7aedfe4f7b
Shino <shinoshaji13@gmail.com>
2023-08-29 15:45:53 +0530

    Created Notes App

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Header() {
+  return (
+    <header>
+      <h1><img width="32" height="32"  src="https://img.icons8.com/doodle/48/apple-notes.png" alt="apple-notes"/>Notes</h1>
+    </header>
+  );
+}
+
+export default Header;
